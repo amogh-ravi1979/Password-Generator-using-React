c7812ca446a526a1dd1fd479d2143de8c5e061ce
amogh-ravi1979 <amogh2006r@gmail.com>
2026-05-13 12:05:46 +0530

    Replace App component with PasswordGenerator component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import PasswordGenerator from "./PasswordGenerator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PasswordGenerator />
   </React.StrictMode>
 );
